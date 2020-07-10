0d986ecf5a30465c45dfe8cd73d4790ad186e5d3
gureis <gureis4@gmail.com>
2020-07-10 18:06:20 -0300

    refactor 'largura' specific functions to a separate file

---
+++ largura.js
@@ -0,0 +1,18 @@
+const criaMatriz = require(`./Matriz`);
+const trocarElementos = require(`./utils`).trocarElementos;
+
+function expandirNo(matriz, posZero, movimentosDisponiveis) {
+    let matrizesExapandidas = new Array();
+
+    movimentosDisponiveis.forEach(movimentoDisponivel => {
+        const novoNo = trocarElementos(matriz.getMatriz(), posZero, movimentoDisponivel);
+        const novaMatriz = criaMatriz(novoNo, matriz);
+        matrizesExapandidas.push(novaMatriz);
+    });
+
+    return matrizesExapandidas;
+}
+
+module.exports = {
+    expandirNo
+}
